284ffc6bb4e42af922b9e1c39e2b3494350064ba
Fabio Nukui <fabio.nukui@gmail.com>
2021-04-01 18:04:57 -0300

    changed default remote endpoint

--- src/configs.py
+++ src/configs.py
@@ -3,7 +3,7 @@ import os
 LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO')
 LOG_AWS = os.getenv('LOG_AWS') == 'True'
 
-RCP_REMOTE_URI = os.getenv('RCP_REMOTE_URI', 'wss://dummy.com')
+RCP_REMOTE_URI = os.getenv('RCP_REMOTE_URI', 'wss://bsc-ws-node.nariox.org:443')
 RCP_LOCAL_URI = os.getenv('RCP_LOCAL_URI', 'geth.ipc')
 
 PRIVATE_KEY = os.environ['PRIVATE_KEY']
