371756063ebcb85124d28c91edbd37e794f9fd94
Fabio Nukui <fabio.nukui@gmail.com>
2021-04-01 21:23:16 +0000

    wqadded helper scripts

---
+++ scripts/downloader.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+import asyncio
+import concurrent.futures
+import os
+import time
+from random import random
+
+import requests
+from tqdm import tqdm
+
+URL = 'https://s3.ap-northeast-1.amazonaws.com/dex-bin.bnbstatic.com/s3-witness-data-download/chaindata_202103.zip?AWSAccessKeyId=AKIAYINE6SBQPUZDDRRO&Expires=1641450253&Signature=hOC8I8HSpCOytlYMVQwKRc5oUaI%3D'  # noqa: E501
+OUTPUT = 'chaindata_202103.zip'
+DOWNLOAD_JIGGLE_TIME = 10
+N_WORKERS = 60
+BLOCK_SIZE_DISK_WRITE = 10_2400  # 100 KB
+BLOCK_SIZE_DOWNLOAD = 500_000_000  # 500 MiB
+N_TRIES = 3
+
+
+async def get_size(url):
+    response = requests.get(url, stream=True)
+    size = int(response.headers['Content-Length'])
+    return size
+
+
+def download_range(url, start, end, output):
+    if os.path.exists(output):
+        print(f'Skipping {output}')
+        return
+    time.sleep(random() * DOWNLOAD_JIGGLE_TIME)
+    print(f'Starting {output}')
+    headers = {'Range': f'bytes={start}-{end}'}
+    response = requests.get(url, stream=True, headers=headers)
+
+    for _ in range(N_TRIES):
+        try:
+            with open(f'{output}_incomplete', 'wb') as f:
+                for part in response.iter_content(BLOCK_SIZE_DISK_WRITE):
+                    f.write(part)
+        except Exception:
+            pass
+        else:
+            os.rename(f'{output}_incomplete', output)
+            break
+        finally:
+            if os.path.exists(f'{output}_incomplete'):
+                os.remove(f'{output}_incomplete')
+    print(f'Finished {output}')
+
+
+async def download(executor, url, output, chunk_size=BLOCK_SIZE_DOWNLOAD):
+    loop = asyncio.get_event_loop()
+
+    file_size = await get_size(url)
+    chunks = range(0, file_size, chunk_size)
+
+    tasks = [
+        loop.run_in_executor(
+            executor,
+            download_range,
+            url,
+            start,
+            start + chunk_size - 1,
+            f'{output}.part{i:04d}',
+        )
+        for i, start in enumerate(chunks)
+    ]
+
+    await asyncio.wait(tasks)
+
+    print('Finished downloads, consolidating parts')
+    with open(output, 'wb') as o:
+        for i in tqdm(range(len(chunks))):
+            chunk_path = f'{output}.part{i:04d}'
+
+            with open(chunk_path, 'rb') as s:
+                o.write(s.read())
+            os.remove(chunk_path)
+    print('Finished')
+
+
+if __name__ == '__main__':
+    executor = concurrent.futures.ThreadPoolExecutor(max_workers=N_WORKERS)
+    asyncio.run(download(executor, URL, OUTPUT))
---
+++ scripts/estimate_sync_time.py
@@ -0,0 +1,131 @@
+import pathlib
+import re
+import time
+from datetime import datetime
+
+POOL_INTERVAL = 1
+PAT = re.compile(r'^t=(?P<t>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}).+msg="Imported new chain segment".+\bnumber=(?P<n>\d+)\b')  # noqa: E501
+N_LINES_BUFFER = 100
+
+SECONDS_PER_BLOCK = 3
+REFERENCE_BLOCK = {
+    't': datetime.fromisoformat('2021-03-31T00:56:38'),
+    'n': 6146421
+}
+
+DIR_PATH = pathlib.Path(__file__).parent / 'node'
+PREFIX = 'bsc.log.'
+
+
+class Logs:
+    def __init__(self):
+        self.log_files = sorted(self.get_files())
+        self.lines = [
+            line
+            for file in self.log_files[:-1]
+            for line in open(file).readlines()
+        ]
+        self.file = open(self.log_files[-1])
+
+    def get_files(self):
+        return (
+            f for f in DIR_PATH.iterdir()
+            if f.is_file() and not f.is_symlink() and f.name.startswith(PREFIX)
+        )
+
+    def update_newer_file(self):
+        newest_file = max(self.get_files())
+        if str(newest_file) == self.file.name:
+            return
+        self.file.close()
+        self.log_files.append(self.file.name)
+        self.file = open(newest_file)
+
+    def iterlines(self):
+        for line in self.lines:
+            yield line
+        self.lines = []
+        while True:
+            where = self.file.tell()
+            line = self.file.readline()
+            if line:
+                yield line
+                self.update_newer_file()
+            else:
+                time.sleep(POOL_INTERVAL)
+                self.file.seek(where)
+
+    def iterdata(self):
+        for line in self.iterlines():
+            match = PAT.search(line)
+            if not match:
+                continue
+            d = match.groupdict()
+            yield {
+                't': datetime.fromisoformat(d['t']),
+                'n': int(d['n'])
+            }
+
+
+class Blocks:
+    def __init__(self):
+        self.blocks = []
+
+    def load_new_block(self, block):
+        self.blocks.append(block)
+        self.blocks = self.blocks[-100:]
+
+    @property
+    def oldest_processed_block(self):
+        return self.blocks[0]
+
+    @property
+    def last_processed_block(self):
+        return self.blocks[-1]
+
+    def get_blocks_per_second(self) -> float:
+        if self.oldest_processed_block == self.last_processed_block:
+            return float('nan')
+        delta_time = self.oldest_processed_block['t'] - self.last_processed_block['t']
+        delta_seconds = delta_time.total_seconds()
+        delta_blocks = self.oldest_processed_block['n'] - self.last_processed_block['n']
+        return delta_blocks / delta_seconds
+
+    @property
+    def latest_block(self):
+        now = datetime.utcnow()
+        seconds_from_reference = (now - REFERENCE_BLOCK['t']).total_seconds()
+        block = int(REFERENCE_BLOCK['n'] + seconds_from_reference / SECONDS_PER_BLOCK)
+        return {
+            't': now,
+            'n': block
+        }
+
+    @property
+    def blocks_remaining(self):
+        return self.latest_block['n'] - self.last_processed_block['n']
+
+    def get_hours_remaining(self):
+        blocks_per_second = self.get_blocks_per_second()
+        if blocks_per_second <= 1 / SECONDS_PER_BLOCK:
+            return float('inf')
+        return self.blocks_remaining / (blocks_per_second - 1 / SECONDS_PER_BLOCK) / 3600
+
+
+def main():
+    logs = Logs()
+    blocks = Blocks()
+    for d in logs.iterdata():
+        blocks.load_new_block(d)
+        time_left = blocks.get_hours_remaining()
+        log = (
+            f'{blocks.last_processed_block["t"].isoformat()}: '
+            f'blocks left: {blocks.blocks_remaining}; '
+            f'blocks/s: {blocks.get_blocks_per_second():,.2f}; '
+            f'time left: {time_left:,.2f}; '
+        )
+        print(log)
+
+
+if __name__ == '__main__':
+    main()
