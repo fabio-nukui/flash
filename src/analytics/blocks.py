6650bcdf53fa9ba3418054720f82494b50ad8c92
Fabio Nukui <fabio.nukui@gmail.com>
2021-04-09 20:13:40 -0300

    added optional provider argument

--- src/analytics/blocks.py
+++ src/analytics/blocks.py
@@ -1,25 +1,26 @@
 import pandas as pd
+from web3 import Web3
 
 import tools
 
 WEB3 = tools.w3.get_web3()
 
 
-def get_receipts(block_number: int) -> pd.DataFrame:
+def get_receipts(block_number: int, web3: Web3 = WEB3) -> pd.DataFrame:
     data = [
-        WEB3.eth.getTransactionReceipt(tx)
-        for tx in WEB3.eth.get_block(block_number).transactions
+        web3.eth.getTransactionReceipt(tx)
+        for tx in web3.eth.get_block(block_number).transactions
     ]
     return pd.DataFrame(data)
 
 
-def get_gas_data(block_number: int) -> pd.DataFrame:
+def get_gas_data(block_number: int, web3: Web3 = WEB3) -> pd.DataFrame:
     data = [
         {
             'tx': tx.hex(),
-            'price_gwei': WEB3.eth.getTransaction(tx).gasPrice / 10**9,
-            'gas': WEB3.eth.getTransactionReceipt(tx).gasUsed
+            'price_gwei': web3.eth.getTransaction(tx).gasPrice / 10**9,
+            'gas': web3.eth.getTransactionReceipt(tx).gasUsed
         }
-        for tx in WEB3.eth.get_block(block_number).transactions
+        for tx in web3.eth.get_block(block_number).transactions
     ]
     return pd.DataFrame(data)
