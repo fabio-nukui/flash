63cad098d0528c89987c3e85470977dcef454ada
Fabio Nukui <fabio.nukui@gmail.com>
2021-04-23 03:25:55 +0000

    added DexProtocol to dex namespace

--- src/dex/__init__.py
+++ src/dex/__init__.py
@@ -1,10 +1,11 @@
-from .curve import EllipsisDex, CurveTrade
+from .base import DexProtocol
+from .curve import CurveTrade, EllipsisDex
 from .uniswap_v2 import MDex, PancakeswapDex
 from .valuedefi import ValueDefiSwapDex
 
-
 __all__ = [
     'CurveTrade',
+    'DexProtocol',
     'EllipsisDex',
     'MDex',
     'PancakeswapDex',
