dd81db6895c99310ec1825a81aa2debdfb436733
Fabio Nukui <fabio.nukui@gmail.com>
2021-04-23 03:45:20 +0000

    updated http retrier

--- src/tools/http.py
+++ src/tools/http.py
@@ -1,19 +1,31 @@
 import httpx
 
+import logging
+
+log = logging.getLogger(__name__)
+
 
 def get(url, *args, n_tries_: int = 3, **kwargs):
     """httpx GET with default retries"""
     for i in range(n_tries_):
-        res = httpx.get(url, *args, **kwargs)
-        res.raise_for_status()
-        return res
-    raise Exception(f'GET failed after {n_tries_} tries')
+        try:
+            res = httpx.get(url, *args, **kwargs)
+            res.raise_for_status()
+            return res
+        except Exception as e:
+            if i == n_tries_ - 1:
+                raise e
+            log.debug(f'Error on http GET:{e:!r}', exc_info=True)
 
 
 def post(url, *args, n_tries_: int = 3, **kwargs):
     """httpx POST with default retries"""
     for i in range(n_tries_):
-        res = httpx.post(url, *args, **kwargs)
-        res.raise_for_status()
-        return res
-    raise Exception(f'POST failed after {n_tries_} tries')
+        try:
+            res = httpx.post(url, *args, **kwargs)
+            res.raise_for_status()
+            return res
+        except Exception as e:
+            if i == n_tries_ - 1:
+                raise e
+            log.debug(f'Error on http POST:{e:!r}', exc_info=True)
