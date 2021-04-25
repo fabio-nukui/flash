059d42365922c5fff41e94f5c366a54010e33658
Fabio Nukui <fabio.nukui@gmail.com>
2021-04-25 00:19:03 -0300

    finished contract

--- contracts/libraries/AddressArrayEncoder.sol
+++ contracts/libraries/AddressArrayEncoder.sol
@@ -10,6 +10,13 @@ library AddressArrayEncoder{
         }
     }
 
+    function bytesToHeader(bytes calldata data) internal pure returns (uint8 header) {
+        bytes memory b = data;
+        assembly {
+          header := mload(add(b, 8))
+        }
+    }
+
     function decode(bytes calldata data) internal pure returns(address[] memory addresses) {
         uint n = data.length / 20;
         addresses = new address[](n);
@@ -21,6 +28,24 @@ library AddressArrayEncoder{
 
     function encode(address[] calldata addresses) internal pure returns(bytes memory data) {
         data = new bytes(1);
+        for(uint i = 0; i < addresses.length; i++){
+            data = abi.encodePacked(data, addresses[i]);
+        }
+    }
+
+    function decodeWithHeader(bytes calldata data) internal pure returns(uint8 header, address[] memory addresses) {
+        uint n = data.length / 20;
+        addresses = new address[](n);
+
+        header = bytesToHeader(data);
+
+        for(uint i = 0; i < n; i++){
+            addresses[i] = bytesToAddress(data[i * 20 + 8:(i + 1) * 20 + 8]);
+        }
+    }
+
+    function encodeWithHeader(uint8 header, address[] calldata addresses) internal pure returns(bytes memory data) {
+        data = abi.encodePacked(header);
         for(uint i = 0; i < addresses.length; i++){
             data = abi.encodePacked(data, addresses[i]);
         }
--- contracts/strategies/PcsPcs2V1.sol
+++ contracts/strategies/PcsPcs2V1.sol
@@ -13,11 +13,11 @@ import {PancakeswapLibrary} from "../libraries/uniswap_v2/PancakeswapLibrary.sol
 import {MdexLibrary} from "../libraries/uniswap_v2/MdexLibrary.sol";
 
 
-contract PcsMdxV1 is Withdrawable, CHIBurner {
+contract PcsPcs2V1 is Withdrawable, CHIBurner {
     address constant pcs1Factory = 0xBCfCcbde45cE874adCB698cC183deBcF17952812;
     address constant pcs2Factory = 0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73;
-    bytes constant PCS_1_FIRST = 0;
-    bytes constant PCS_2_FIRST = 1;
+    uint8 constant PCS_1_FIRST = 0;
+    uint8 constant PCS_2_FIRST = 1;
 
     // PcsV1 first
     function swapPcs1First(
@@ -29,7 +29,7 @@ contract PcsMdxV1 is Withdrawable, CHIBurner {
         uint256 amount0Out = token0 == path[path.length - 1] ? amountLast : 0;
         uint256 amount1Out = token0 == path[path.length - 1] ? 0 : amountLast;
 
-        bytes memory data = AddressArrayEncoder.encode_with_header(PCS_1_FIRST, path);
+        bytes memory data = AddressArrayEncoder.encodeWithHeader(PCS_1_FIRST, path);
         IUniswapV2Pair(pcs1Pair).swap(amount0Out, amount1Out, address(this), data);
     }
 
@@ -43,7 +43,7 @@ contract PcsMdxV1 is Withdrawable, CHIBurner {
         uint256 amount0Out = token0 == path[path.length - 1] ? amountLast : 0;
         uint256 amount1Out = token0 == path[path.length - 1] ? 0 : amountLast;
 
-        bytes memory data = AddressArrayEncoder.encode_with_header(PCS_2_FIRST, path);
+        bytes memory data = AddressArrayEncoder.encodeWithHeader(PCS_2_FIRST, path);
         IUniswapV2Pair(pcs2Pair).swap(amount0Out, amount1Out, address(this), data);
     }
 
@@ -53,27 +53,30 @@ contract PcsMdxV1 is Withdrawable, CHIBurner {
         uint256 amount1,
         bytes calldata data
     ) external {
-        address[] memory path = AddressArrayEncoder.decode(data);
-        uint256 amountSendPcs2 = amount0 == 0 ? amount1 : amount0;
+        (uint8 mode, address[] memory path) = AddressArrayEncoder.decodeWithHeader(data);
+        uint256 amountSendPair = amount0 == 0 ? amount1 : amount0;
 
-        uint256[] memory amounts = PancakeswapLibrary.getAmountsIn(pcsFactory, amountSendPcs2, path);
-        exchangePcs(path[path.length - 1], path[0], amountSendPcs2, amounts[0]);
+        (address firstFactory, address secondFactory) = mode == PCS_1_FIRST ? (pcs1Factory, pcs2Factory) : (pcs2Factory, pcs1Factory);
 
-        address firstPair = PancakeswapLibrary.pairFor(pcsFactory, path[0], path[1]);
+        uint256[] memory amounts = PancakeswapLibrary.getAmountsIn(firstFactory, amountSendPair, path);
+        exchangePcs(secondFactory, path[path.length - 1], path[0], amountSendPair, amounts[0]);
+
+        address firstPair = PancakeswapLibrary.pairFor(firstFactory, path[0], path[1]);
         TransferHelper.safeTransfer(path[0], firstPair, amounts[0]);
-        _pcs_swap(amounts, path);
+        _pcs_swap(firstFactory, amounts, path);
     }
 
     function exchangePcs(
+        address factory,
         address tokenIn,
         address tokenOut,
         uint256 amountIn,
         uint256 amountOutMin
-    ) private {
-        address pairAddress = PancakeswapLibrary.pairFor(pcsFactory, tokenIn, tokenOut);
+    ) internal {
+        address pairAddress = PancakeswapLibrary.pairFor(factory, tokenIn, tokenOut);
         IUniswapV2Pair pair = IUniswapV2Pair(pairAddress);
 
-        (uint reserveIn, uint reserveOut) = PancakeswapLibrary.getReserves(pcsFactory, tokenIn, tokenOut);
+        (uint reserveIn, uint reserveOut) = PancakeswapLibrary.getReserves(factory, tokenIn, tokenOut);
         uint256 amountOut = PancakeswapLibrary.getAmountOut(amountIn, reserveIn, reserveOut);
         require(amountOut > amountOutMin, 'PCS low return');
         TransferHelper.safeApproveAndTransfer(tokenIn, pairAddress, amountIn);
@@ -82,14 +85,14 @@ contract PcsMdxV1 is Withdrawable, CHIBurner {
         pair.swap(amount0Out, amount1Out, address(this), new bytes(0));
     }
 
-    function _pcs_swap(uint[] memory amounts, address[] memory path) internal {
+    function _pcs_swap(address factory, uint[] memory amounts, address[] memory path) internal {
         for (uint i; i < path.length - 2; i++) {
             (address input, address output) = (path[i], path[i + 1]);
             (address token0,) = PancakeswapLibrary.sortTokens(input, output);
             uint amountOut = amounts[i + 1];
             (uint amount0Out, uint amount1Out) = input == token0 ? (uint(0), amountOut) : (amountOut, uint(0));
-            address to = PancakeswapLibrary.pairFor(pcsFactory, output, path[i + 2]);
-            IUniswapV2Pair(PancakeswapLibrary.pairFor(pcsFactory, input, output)).swap(amount0Out, amount1Out, to, new bytes(0));
+            address to = PancakeswapLibrary.pairFor(factory, output, path[i + 2]);
+            IUniswapV2Pair(PancakeswapLibrary.pairFor(factory, input, output)).swap(amount0Out, amount1Out, to, new bytes(0));
         }
     }
 }
