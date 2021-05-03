4cddaeca551c4ed0c5573f6de27c27ea2f85b2f2
Fabio Nukui <fabio.nukui@gmail.com>
2021-05-03 10:11:41 +0000

    adjusted alpaca share of profit

--- src/strategies/pcs_pcs2_v2.py
+++ src/strategies/pcs_pcs2_v2.py
@@ -62,8 +62,8 @@ def get_share_of_profit(params: dict):
     reduced_gas_share_pools = {
         '0xfC207DB720851f52545229E406068b205E02B952': 0.01,  # pcs xBLZD/WBNB
         '0xD9002B7E7d63A71F04a16840DA028e1cd534889D': 0.01,  # pcs2 xBLZD/WBNB
-        '0x3Ee4de968E47877F432226d6a9A0DAD6EAc6001b': 0.21,  # pcs sALPACA/ALPACA
-        '0x6615187234104CE7d2fb1deF75eDb9d77408230D': 0.21,  # pcs2 sALPACA/ALPACA
+        '0x3Ee4de968E47877F432226d6a9A0DAD6EAc6001b': 0.14,  # pcs sALPACA/ALPACA
+        '0x6615187234104CE7d2fb1deF75eDb9d77408230D': 0.14,  # pcs2 sALPACA/ALPACA
     }
     for pool in params['first_route'].pools + params['second_route'].pools:
         if pool.address in reduced_gas_share_pools:
