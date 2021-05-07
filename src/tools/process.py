5c79bfc8cdc242acd1f599be7d37aaa0c9a9e78f
Fabio Nukui <fabio.nukui@gmail.com>
2021-05-07 07:03:02 +0000

    added missing args

--- src/tools/process.py
+++ src/tools/process.py
@@ -16,7 +16,7 @@ def is_shutting_down() -> bool:
         return _is_shutting_down
 
 
-def set_shutting_down_flag():
+def set_shutting_down_flag(*args):
     log.info('Shutting down')
     global _is_shutting_down
     with _lock:
