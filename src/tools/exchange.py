c09f79ffd0f202ee6efe0a37f388a5dd9bab8d61
Fabio Nukui <fabio.nukui@gmail.com>
2021-05-10 12:30:09 -0300

    added optional account parameter

--- src/tools/exchange.py
+++ src/tools/exchange.py
@@ -2,11 +2,11 @@ import json
 import logging
 from typing import Union
 
-from web3 import Web3
+from web3 import Account, Web3
 
 import configs
 from core import Token, TokenAmount
-from tools import transaction, http, price
+from tools import http, price, transaction
 
 log = logging.getLogger(__name__)
 
@@ -23,7 +23,7 @@ with open('addresses/dex/_1inch_v3/_1inch_v3.json') as f:
 def get_quote_1inch(
     amountIn: Union[int, TokenAmount],
     tokenOut: Token = None,
-    gas_price: int = None
+    gas_price: int = None,
 ) -> int:
     token_out_address = tokenOut.address if tokenOut is not None else _1INCH_CURRENCY_ADDRESS
     gas_price = price.get_gas_price() if gas_price is None else gas_price
@@ -53,14 +53,16 @@ def exchange_1inch(
     gas_price: int = None,
     wait_finish: bool = False,
     max_blocks_wait_: int = None,
+    account: Account = None,
 ):
     token_out_address = tokenOut.address if tokenOut is not None else _1INCH_CURRENCY_ADDRESS
     gas_price = price.get_gas_price() if gas_price is None else gas_price
+    address = configs.ADDRESS if account is None else account.address
     if isinstance(amountIn, TokenAmount):
         from_token_address = amountIn.token.address
         from_token_amount = amountIn.amount
         allowance = (
-            amountIn.token.contract.functions.allowance(configs.ADDRESS, _1INCH_ROUTER_ADDRESS)
+            amountIn.token.contract.functions.allowance(address, _1INCH_ROUTER_ADDRESS)
             .call(block_identifier=configs.BLOCK)
         )
         if allowance < from_token_amount:
@@ -68,7 +70,8 @@ def exchange_1inch(
                 amountIn.token.contract.functions.approve,
                 _1INCH_ROUTER_ADDRESS,
                 amountIn.amount,
-                wait_finish_=True
+                wait_finish_=True,
+                account=account,
             )
             log.debug(f'Added {amountIn} allowance to 1inch ({tx_hash})')
     else:
@@ -78,7 +81,7 @@ def exchange_1inch(
         'fromTokenAddress': from_token_address,
         'toTokenAddress': token_out_address,
         'amount': from_token_amount,
-        'fromAddress': configs.ADDRESS,
+        'fromAddress': address,
         'slippage': max_slippage,
         'gasPrice': gas_price,
         'allowPartialFill': True,
@@ -95,4 +98,4 @@ def exchange_1inch(
     tx['gasPrice'] = int(tx['gasPrice'])
     tx['to'] = web3.toChecksumAddress(tx['to'])
 
-    return transaction.sign_and_send_tx(tx, web3, wait_finish, max_blocks_wait_)
+    return transaction.sign_and_send_tx(tx, web3, wait_finish, max_blocks_wait_, account=account)
